5704e553da13b8e6cdc88b6a3ffee23574df9de8
Lucas P Siqueira <siqueiralpontes@gmail.com>
2024-10-03 12:22:27 -0300

    first chapter: config simple app and routes

---
+++ market.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
---
+++ start_app.ps1
@@ -0,0 +1,3 @@
+$env:FLASK_APP=".\market.py"
+$env:FLASK_DEBUG=1
+flask run
