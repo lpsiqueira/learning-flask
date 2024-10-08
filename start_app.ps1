a2c76551f2a5d9325451e977b4bec1be57d6d4b2
Lucas Siqueira <siqueiralpontes@gmail.com>
2024-10-08 10:25:57 -0300

    some project configuration changes

--- start_app.ps1
+++ start_app.ps1
@@ -1,3 +1,37 @@
 $env:FLASK_APP=".\src\market.py"
-$env:FLASK_DEBUG=1
-flask run
+function activateVenv {
+    .\venv\Scripts\Activate.ps1
+}
+function runApp {
+	$env:FLASK_DEBUG=1
+	flask run
+}
+
+function flaskShell {
+	flask shell
+}
+
+
+if ($args.Count -eq 0) {
+    Write-Host "Please provide an argument."
+    exit
+}
+
+$arg = $args[0]
+switch ($arg.ToLower()) {
+    'venv' {
+        activateVenv
+    }
+    
+	'run' {
+        runApp
+    }
+
+    'shell' {
+        flaskShell
+    }
+
+    default {
+        Write-Host "Invalid argument."
+    }
+}
